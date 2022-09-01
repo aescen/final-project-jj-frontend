64111b64bd09929b381c9ea3c62a8776d27729bf
aescen <ash.putra15@gmail.com>
2022-09-02 05:08:13 +0700

    feat: fixup for build

--- src/components/vendor/settings/Settings.js
+++ src/components/vendor/settings/Settings.js
@@ -1,7 +1,7 @@
 import Layout from '../layout/Layout';
 import cs from '../../../assets/undraw_under_construction.svg';
 
-const SalesReport = () => {
+const Settings = () => {
   return (
     <Layout>
       <div
@@ -11,11 +11,11 @@ const SalesReport = () => {
           backgroundSize: 'contain',
         }}
       >
-        <div className='fs-1 fw-semibold mt-5'>Sales Report</div>
+        <div className='fs-1 fw-semibold mt-5'>Settings</div>
         <div className='fs-3 mt-5'>Coming soon</div>
       </div>
     </Layout>
   );
 };
 
-export default SalesReport;
+export default Settings;
